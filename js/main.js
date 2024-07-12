bed914e0f3ec929ec448b79550044856a9a3f47b
qpwoei0123 <qpwoei0123@naver.com>
2024-07-12 19:24:07 +0900

    refactor: main.js jsDoc 적용

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 import { projects, footerLinks } from "./data.js";
 
-// HTML 요소를 생성하는 함수
+/**
+ * HTML 요소를 생성하는 함수
+ *
+ * @param {string} tag - 생성할 HTML 태그 이름.
+ * @param {string} [className] - 요소에 추가할 클래스 이름.
+ * @param {string} [textContent] - 요소에 설정할 텍스트 내용.
+ * @returns {HTMLElement} 생성된 HTML 요소.
+ */
 const createElement = (tag, className, textContent) => {
   const element = document.createElement(tag);
   if (className) element.className = className;
@@ -8,7 +15,14 @@ const createElement = (tag, className, textContent) => {
   return element;
 };
 
-// 프로젝트의 헤더를 생성하는 함수
+/**
+ * 프로젝트의 헤더를 생성하는 함수
+ *
+ * @param {Object} project - 프로젝트 객체.
+ * @param {string} project.title - 프로젝트 제목.
+ * @param {string} project.created - 프로젝트 생성 날짜.
+ * @returns {HTMLElement} 생성된 헤더 요소.
+ */
 const createHeader = ({ title, created }) => {
   const header = createElement("div", "header");
   header.appendChild(createElement("h3", null, title));
@@ -16,7 +30,15 @@ const createHeader = ({ title, created }) => {
   return header;
 };
 
-// 프로젝트의 본문을 생성하는 함수
+/**
+ * 프로젝트의 본문을 생성하는 함수
+ *
+ * @param {Object} project - 프로젝트 객체.
+ * @param {string} project.title - 프로젝트 제목.
+ * @param {string} project.desc - 프로젝트 설명.
+ * @param {string} project.link - 프로젝트 링크.
+ * @returns {HTMLElement} 생성된 본문 요소.
+ */
 const createBody = ({ title, desc, link }) => {
   const body = createElement("div", "body");
 
@@ -35,7 +57,12 @@ const createBody = ({ title, desc, link }) => {
   return body;
 };
 
-// 프로젝트 요소를 생성하는 함수
+/**
+ * 프로젝트 요소를 생성하는 함수
+ *
+ * @param {Object} project - 프로젝트 객체.
+ * @returns {HTMLElement} 생성된 프로젝트 요소.
+ */
 const createProjectElement = (project) => {
   const li = createElement("li");
   li.appendChild(createHeader(project));
@@ -43,14 +70,25 @@ const createProjectElement = (project) => {
   return li;
 };
 
-// 커서 이미지를 생성하는 함수
+/**
+ * 커서 이미지를 생성하는 함수
+ *
+ * @param {string} title - 이미지 파일 이름(확장자 제외).
+ * @returns {HTMLElement} 생성된 이미지 요소.
+ */
 const createCursorImage = (title) => {
   const cursorImg = createElement("img");
   cursorImg.src = `./images/${title}.png`;
   return cursorImg;
 };
 
-// 프로젝트를 렌더링하는 함수
+/**
+ * 프로젝트를 렌더링하는 함수
+ *
+ * @param {Object[]} projects - 프로젝트 객체 배열.
+ * @param {HTMLElement} projectList - 프로젝트 리스트 요소.
+ * @param {HTMLElement} imgWarp - 커서 이미지 래퍼 요소.
+ */
 const renderProjects = (projects, projectList, imgWarp) => {
   Object.values(projects).forEach((project) => {
     projectList.appendChild(createProjectElement(project));
@@ -58,7 +96,14 @@ const renderProjects = (projects, projectList, imgWarp) => {
   });
 };
 
-// 푸터 아이콘을 생성하는 함수
+/**
+ * 푸터 아이콘을 생성하는 함수
+ *
+ * @param {Object} link - 링크 객체.
+ * @param {string} link.icon - 아이콘 클래스 이름.
+ * @param {string} link.link - 링크 URL.
+ * @returns {HTMLElement} 생성된 아이콘 요소.
+ */
 const createFooterIcon = ({ icon, link }) => {
   const i = createElement("i", icon);
   i.addEventListener("click", () => window.open(link, "_blank"));
@@ -67,7 +112,12 @@ const createFooterIcon = ({ icon, link }) => {
   return div;
 };
 
-// 푸터 링크를 렌더링하는 함수
+/**
+ * 푸터 링크를 렌더링하는 함수
+ *
+ * @param {Object[]} footerLinks - 푸터 링크 객체 배열.
+ * @param {HTMLElement} footerIconBox - 푸터 아이콘 박스 요소.
+ */
 const renderFooterLinks = (footerLinks, footerIconBox) => {
   Object.values(footerLinks).forEach((link) =>
     footerIconBox.appendChild(createFooterIcon(link))
