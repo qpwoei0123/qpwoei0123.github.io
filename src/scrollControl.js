7d8edbe5a0df6ced1c062a1d1f74fc95b266c0f7
qpwoei0123 <qpwoei0123@naver.com>
2024-08-01 14:58:07 +0900

    refactor: 스크롤 컨트롤 안정화

--- src/scrollControl.js
+++ src/scrollControl.js
@@ -1,9 +1,12 @@
 // 3초 뒤에 스크롤 가능
-setTimeout(() => {
-  document.documentElement.style.overflowY = "scroll";
-}, 3000);
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.documentElement.style.overflowY = "scroll";
+  }, 3000);
+});
 
 // 새로고침 시 스크롤 위치 초기화
 window.onbeforeunload = (e) => {
   window.scrollTo(0, 0);
+  document.documentElement.style.overflowY = "hidden";
 };
